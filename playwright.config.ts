51b61fe2e2b0645598625c3707cf290d195c2b65
chris <cmseeling@gmail.com>
2023-12-09 10:05:21 -0600

    use out of the box html reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['playwright-html', { outputFolder: 'test-results', minifyAssets: true, startServer: false}]],
+  // reporter: [['playwright-html', { outputFolder: 'test-results', minifyAssets: true, startServer: false}]],
+  reporter: [['html', { outputDir: 'test-results', outputFile: 'index.html' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
